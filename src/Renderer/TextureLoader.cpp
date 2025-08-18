0fc674b4eb9daa9accd09dbf2e6bdedf2d3110ca
William <wccarricaburu@icloud.com>
2025-08-17 21:42:01 -0700

    Set minLod to a valid mip level to stop extreme low res rendering

--- src/Renderer/TextureLoader.cpp
+++ src/Renderer/TextureLoader.cpp
@@ -67,7 +67,7 @@ namespace renderer{
         samplerInfo.compareEnable = VK_FALSE;
         samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
         samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-        samplerInfo.minLod = static_cast<float>(mipLevels);
+        samplerInfo.minLod = 0.0f;
         samplerInfo.maxLod = VK_LOD_CLAMP_NONE;
         samplerInfo.mipLodBias = 0.0f;
 
